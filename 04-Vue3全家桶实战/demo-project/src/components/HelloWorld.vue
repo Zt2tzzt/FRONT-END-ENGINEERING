cb9356f6ad047a4e2f549c7cfd65820504fcf105
ZeT1an <zetian.zhu@cimc.com>
2022-09-09 22:55:32 +0800

    feat: (04-Vue3全家桶实战) 06-父子组件通信-插槽; fix: v28;

--- 04-Vue3全家桶实战/demo-project/src/components/HelloWorld.vue
+++ 04-Vue3全家桶实战/demo-project/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ showMsg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,7 +33,7 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    showMsg: String
   }
 }
 </script>
