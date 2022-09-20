cde5b4308c77d098f4a65fe4943adfc22c5b5ead
ZeT1an <zetian.zhu@cimc.com>
2022-09-20 21:20:45 +0800

    fix: v37;

---
+++ 04-Vue3全家桶实战/demo-project/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+const store = createStore({
+	state:() => ({
+    rootCounter: 100
+	}),
+	mutations: {
+		increment(state) {
+			state.rootCounter++
+		},
+		decrement(state) {
+			state.rootCounter--
+		},
+	}
+})
+export default store
